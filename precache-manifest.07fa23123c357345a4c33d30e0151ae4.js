f1394748e30634fbd9c9ecd2b911af3709f56da3
Arnoldo Marin <arnoldo.emarin@gmail.com>
2020-11-05 17:07:13 -0700

    Updates

---
+++ precache-manifest.07fa23123c357345a4c33d30e0151ae4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "453b0bfbec709386429e84d641993905",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63b0c09a9e4d36653527",
+    "url": "/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "311177c897654495d7ab",
+    "url": "/static/css/main.bd342027.chunk.css"
+  },
+  {
+    "revision": "63b0c09a9e4d36653527",
+    "url": "/static/js/2.a88c6d7c.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.a88c6d7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "311177c897654495d7ab",
+    "url": "/static/js/main.9ae97236.chunk.js"
+  },
+  {
+    "revision": "f57bd6b3cd488554ebce",
+    "url": "/static/js/runtime-main.39870823.js"
+  }
+]);
