430596bb5ba3b3734f3aeba3e1622c3badb613c3
Arnoldo Marin <arnoldo.emarin@gmail.com>
2020-11-24 12:49:25 -0700

    Updates

---
+++ precache-manifest.3821748d6dcd352c066beae1be2918ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53947a18e99e0c5d0da54eba7fb27269",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff94e5446d16a987bff3",
+    "url": "/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "5e286eec7cc19c73dc5f",
+    "url": "/static/css/main.2723d922.chunk.css"
+  },
+  {
+    "revision": "ff94e5446d16a987bff3",
+    "url": "/static/js/2.b4adb887.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.b4adb887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e286eec7cc19c73dc5f",
+    "url": "/static/js/main.ba3ad0c4.chunk.js"
+  },
+  {
+    "revision": "f57bd6b3cd488554ebce",
+    "url": "/static/js/runtime-main.39870823.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f636e24da9c157e9378924c5480421b8.js"
+  "/precache-manifest.3821748d6dcd352c066beae1be2918ae.js"
 );
 
 self.addEventListener('message', (event) => {
